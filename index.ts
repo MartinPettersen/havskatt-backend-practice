1425f87bab3a4298a3a9fa59d4ccf5ee97ffad54
MartinPettersen <searchingtheskyfor@gmail.com>
2024-12-10 08:50:59 +0100

    update port

--- index.ts
+++ index.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const app = express()
 app.use(express.json())
 
-
+const PORT = process.env.PORT || 3000;
 
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => {
@@ -85,5 +85,7 @@ mongoose.connect(process.env.MONGODB_URI)
       }
     })
 
-    app.listen(3000)
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    })
   });
